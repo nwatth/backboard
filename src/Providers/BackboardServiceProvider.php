7380080d892d328d1a42acf6e247070b73cd640b
Nantiwat Thongseng <nwatth@gmail.com>
2015-07-14 00:22:30 +0700

    Add empty service provider

---
+++ src/Providers/BackboardServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Nwatth\Backboard\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BackboardServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
